a3b01aba1a2460595aa3721cd063903c41b67b6a
emman@DESKTOP-ACV2NLG <emmanuel.lamas@gmail.com>
2022-06-08 12:59:45 -0300

    1step

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a link="/"> mkt</a>
+      </header>
+      <main>list product</main>
+    </div>
+  );
+}
+
+export default App;
