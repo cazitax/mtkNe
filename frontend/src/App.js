59693040d05b01539674f10827109801aef60591
emman@DESKTOP-ACV2NLG <emmanuel.lamas@gmail.com>
2022-06-10 19:09:45 -0300

    setting up products and basic style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,33 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import data from "./data";
 function App() {
   return (
     <div className="App">
       <header>
         <a link="/"> mkt</a>
       </header>
-      <main>list product</main>
+      <main>
+        <h1>Fetuared datas</h1>
+        <div className="products">
+          {data.product.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong> $ {product.price} </strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,56 @@
+const data = {
+  product: [
+    {
+      name: "nike slim shirt",
+      slug: "nike-slim-shirt",
+      category: "shirts",
+      image: "/image/pj1.jpg",
+      price: 120,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "hight wualitity shirt",
+    },
+
+    {
+      name: "puma slim shirt",
+      slug: "puma-slim-shirt",
+      category: "shirts",
+      image: "/image/pj2.jpg",
+      price: 120,
+      countInStock: 10,
+      brand: "Puma",
+      rating: 4.5,
+      numReviews: 10,
+      description: "hight wualitity shirt",
+    },
+    //   tamaño para imagen 679px x 829px
+    {
+      name: "adidas slim shirt",
+      slug: "adidas-slim-shirt",
+      category: "shirts",
+      image: "/image/pj3.jpg",
+      price: 120,
+      countInStock: 10,
+      brand: "adidas",
+      rating: 4.5,
+      numReviews: 10,
+      description: "hight wualitity shirt",
+    },
+    {
+      name: "rebook slim shirt",
+      slug: "rebook-slim-shirt",
+      category: "shirts",
+      image: "/image/pj1.jpg",
+      price: 120,
+      countInStock: 10,
+      brand: "Rebook",
+      rating: 4.5,
+      numReviews: 10,
+      description: "hight wualitity shirt",
+    },
+  ],
+};
+
+export default data;
