95096c016b718bc155dfce08ce73c27de2906017
emman@DESKTOP-ACV2NLG <emmanuel.lamas@gmail.com>
2022-06-14 19:55:35 -0300

    css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,33 @@ import "./App.css";
 import Homescreen from "./Screens/homescreen";
 import ProductScreen from "./Screens/productscreen";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/"> mkt</Link>
+          <Navbar bg="dark" vairant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>MKT</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<Homescreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<Homescreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
