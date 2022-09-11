249e9819c0d5d3214aa0ca4dbf4a157b33e3b829
emman@DESKTOP-ACV2NLG <emmanuel.lamas@gmail.com>
2022-09-11 18:48:46 -0300

    db-connected

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
 import express from "express";
 import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log("ERROR: " + err.message);
+  });
+// MONGOSE RETURN A PROMISE
 const app = express();
 const port = process.env.PORT || 5000;
 
