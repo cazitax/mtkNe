6286f40192b26e96224cfc9d80cb98ab01121cba
emman@DESKTOP-ACV2NLG <emmanuel.lamas@gmail.com>
2022-09-11 19:47:02 -0300

    changes in db

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import data from "./data.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
 
 dotenv.config();
 mongoose
@@ -14,28 +16,11 @@ mongoose
   });
 // MONGOSE RETURN A PROMISE
 const app = express();
+app.use("/api/seed", seedRouter);
+
 const port = process.env.PORT || 5000;
 
-app.get("/api/products", (req, res) => {
-  res.send(data.product);
-});
-app.get("/api/products/slug/:slug", (req, res) => {
-  const product = data.product.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product not found" });
-  }
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = data.product.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product not found" });
-  }
-});
+app.use("/api/products", productRouter);
 
 app.listen(port, () => {
   console.log(`server running at http://localhost:${port}`);
